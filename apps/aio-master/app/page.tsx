c57e4aa6638189b88e373f4928bf65eac483f51a
arligle <0774543002@163.com>
2024-03-04 11:50:10 +0800

    feat(apps): adjusting the division of application system functions

---
+++ apps/aio-master/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className=" text-center text-red-800">hello aio-master!</div>
+    </>
+  );
+}
--- apps/web-template/app/page.tsx
+++ apps/web-template/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className=" text-center text-red-800">hello org-web!</div>
+      <div className=" text-center text-red-800">hello web-template</div>
     </>
   );
 }
